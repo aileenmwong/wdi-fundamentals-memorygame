75e767d71d24b8a4b48bf7717fe08107a6f90216
Aileen Wong <aileen.m.wong@gmail.com>
2017-07-14 11:59:32 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,65 +1,74 @@
- console.log("Up and running!");
+console.log("Up and Running!")
+//Worked with Sarah Lund on Javascript
 
-/*
-var cardOne = "Queen"
-var cardTwo = "Queen"
-var cardThree = "King"
-var cardFour = "King"
-*/
-
-
+//Create an array to store all card objects
 var cards = [
 {
-  rank: 'queen',
+  card: 'queen',
   suit: 'hearts',
   cardImage: 'images/queen-of-hearts.png'
 },
 {
-  rank: 'queen',
+  card: 'queen',
   suit: 'diamonds',
   cardImage: 'images/queen-of-diamonds.png'
 },
 {
-  rank: 'king',
-  suit: 'hearts',
+  card: 'king',
+  suit: 'diamonds',
   cardImage: 'images/king-of-hearts.png'
 },
 {
-  rank: 'king',
+  card: 'king',
   suit: 'diamonds',
   cardImage: 'images/king-of-diamonds.png'
-}
-
+},
 ];
+
+//Create an array to store cards in play
 var cardsInPlay = [];
 
+//Check to see if cards match
 var checkForMatch = function () {
-  if (cardsInPlay[0] === cardsInPlay[2]) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
   } else {
   alert("Sorry, try again.");
   }
 };
 
-var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId].rank)
-  cardsInPlay.push(cards[cardId].rank);
-  console.log("cardImage");
-  console.log("suit");
-
-//If 2 cards are in play, cards will either match or prompt to play again
-if (cardsInPlay.length === 2) {
-checkForMatch();
+//Create a function to flip card
+var flipCard = function () {
+  //get data-id attribute of the card just clicked and store it in the variable cardID
+  cardId = this.getAttribute('data-id');
+  console.log("User flipped " + cards[cardId])
+  //Add cards to the end of the cardsInPlay array
+  cardsInPlay.push(cards[cardId].card);
+  //Display image of the card just clicked
+  this.setAttribute('src', cards[cardId].cardImage);
+  // Check to see if two cards have been played
+  if (cardsInPlay.length === 2) {
+    // If so, call for checkForMatch
+    checkForMatch();
+  }
 }
-};
-flipCard(0);
-flipCard(2);
 
-
-  /*
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
+// Add a function create your board
+var createBoard = function () {
+  //Within the createBoard function, use a for loop to loop through the cards array
+   for (var i = 0; i < cards.length; i++) {
+    //Create an img element and store it in a variable cardElement
+    var cardElement = document.createElement('img');
+    //Add a src attribute for the back cardElement
+    cardElement.setAttribute('src', 'images/back.png');
+    //set the card's 'data-id' attribute to be the index of the current element, i
+    cardElement.setAttribute('data-id', i);
+    //add a click when the user clicks on a card
+    cardElement.addEventListener('click', flipCard);
+    //append the current cardElement to the game board
+    document.getElementById('game-board').appendChild(cardElement);
+    }
 }
-*/
+
+//Call for createBoard to create game board
+createBoard();
